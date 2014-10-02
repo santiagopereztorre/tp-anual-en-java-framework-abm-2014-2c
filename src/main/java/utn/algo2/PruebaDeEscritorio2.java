bd31f2f606913c1d52830a21dd15bb2df23cfc61
ramabit <f.e.ramirez94@gmail.com>
2014-10-01 21:05:43 -0300

    saco visualizador

--- src/main/java/utn/algo2/PruebaDeEscritorio2.java
+++ src/main/java/utn/algo2/PruebaDeEscritorio2.java
@@ -6,9 +6,7 @@ public class PruebaDeEscritorio2 {
 		
 		Persistidor persistidor = new PersistidorEnMemoria();
 		
-		Visualizador visualizador = new VisualizadorEnVentanas();
-		
-		ABMManager<Persona> abm = new ABMManager<Persona>(persistidor, visualizador);
+		ABMManager<Persona> abm = new ABMManager<Persona>(persistidor);
 		
 		abm.ejecutar();	// El programa queda aca hasta qe se sale de la ventana
 		
