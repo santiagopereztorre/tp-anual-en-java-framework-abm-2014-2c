1087eede2a4e2e464a3204a71e06b6d0f964d062
Martin Montenegro <martin.92m@gmail.com>
2014-10-01 21:13:16 -0300

    Agregada annotations de NotNull

--- src/main/java/utn/algo2/annotations/Control.java
+++ src/main/java/utn/algo2/annotations/Control.java
@@ -3,5 +3,5 @@ package utn.algo2.annotations;
 import utn.algo2.core.controles.ControlBase;
 
 public @interface Control {
-	Class<ControlBase> control();
+	Class<ControlBase> nombre();
 }
---
+++ src/main/java/utn/algo2/annotations/NotNull.java
@@ -0,0 +1,5 @@
+package utn.algo2.annotations;
+
+public @interface NotNull {
+
+}
