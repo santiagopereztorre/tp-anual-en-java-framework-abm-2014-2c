1a47a38564e071acd5216d327ef8abdc216574b2
ramabit <f.e.ramirez94@gmail.com>
2014-10-01 21:23:41 -0300

    uso javadoc

--- src/main/java/utn/algo2/ABMManager.java
+++ src/main/java/utn/algo2/ABMManager.java
@@ -2,40 +2,40 @@ package utn.algo2;
 
 public class ABMManager<T> {
 
-	/*
-	 * DESC: Crea una pantallita donde se muestran los atributos de T, que
-	 * 			se completa con datos y se guarda en una home de T
-	 * RETURN: La T creada
+	/**
+	 * Crea una pantallita donde se muestran los atributos de T, que
+	 * se completa con datos y se guarda en una home de T
+	 * @return La T creada
 	 */
 	public T formularioAlta() {
 		// TODO Auto-generated method stub
 		return null;
 	}
 
-	/*
-	 * DESC: Crea una pantallita igual a la de alta pero con los datos
-	 * 			preestablecidos. Si se modifica, se modifica en el home
-	 * 			tambien.
-	 * RETURN: Si se modifico o no
+	/**
+	 * Crea una pantallita igual a la de alta pero con los datos 
+	 * preestablecidos. Si se modifica, se modifica en el home
+	 * tambien.
+	 * @return Si se modifico o no
 	 */
 	public Boolean formularioModificion(T persona) {
 		// TODO Auto-generated method stub
 		return null;
 	}
 
-	/*
-	 * DESC: Crea una pantallita donde se piden datos segun los atributos
+	/**
+	 * Crea una pantallita donde se piden datos segun los atributos
 	 * 			de T. Tiene un boton buscar que busca en la home de T
 	 * 			segun esos datos
-	 * RETURN: La persona encontrada o null si no encontro nada
+	 * @return La persona encontrada o null si no encontro nada
 	 */
 	public T formularioBusqueda() {
 		// TODO Auto-generated method stub
 		return null;
 	}
 
-	/*
-	 * DESC: Le avisa a la home de T que borre esa T
+	/**
+	 * @return Le avisa a la home de T que borre esa T
 	 */
 	public Boolean borrar(T personaCreada) {
 		// TODO Auto-generated method stub
