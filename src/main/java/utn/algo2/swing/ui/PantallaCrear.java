e60cf1fef6f6ff99140aaf9389edfdaae31b313e
Santiago <saantii.28@hotmail.com>
2014-11-01 20:22:18 -0300

    Pantalla crear y modificar ahora heredan de Pantalla Todo comportamiento comun lo paso a Pantalla

--- src/main/java/utn/algo2/swing/ui/PantallaCrear.java
+++ src/main/java/utn/algo2/swing/ui/PantallaCrear.java
@@ -1,24 +1,15 @@
 package utn.algo2.swing.ui;
 
-import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.lang.reflect.Field;
-import java.util.Hashtable;
 
 import javax.swing.BoxLayout;
 import javax.swing.JButton;
-import javax.swing.JDialog;
 import javax.swing.JLabel;
 import javax.swing.JTextField;
 
-import utn.algo2.core.Entidad;
-
 @SuppressWarnings("serial")
-public class PantallaCrear extends JDialog implements ActionListener {
-
-	private Field[] fields;
-	private Hashtable<Field, JTextField> referenciasATextField = new Hashtable<Field, JTextField>();
-	Entidad entidad = new Entidad();
+public class PantallaCrear extends Pantalla implements ActionListener {
 
 	public PantallaCrear(Field[] fields) {
 		this.fields = fields;
@@ -43,35 +34,5 @@ public class PantallaCrear extends JDialog implements ActionListener {
 		this.add(botonCrear);
 
 		this.configurar();
-	}
-
-	private void configurar() {
-		this.setSize(400, 400);
-		this.setModalityType(ModalityType.APPLICATION_MODAL);
-		this.setVisible(true);
-	}
-
-	public void actionPerformed(ActionEvent e) {
-		synchronized (entidad) {
-			for (Field field : fields) {
-				JTextField textoField = referenciasATextField.get(field);
-				entidad.setValor(field.getName(), textoField.getText());
-			}
-			entidad.notify();
-		}
-		this.setVisible(false);
-	}
-
-	public Entidad getEntidad() {
-		synchronized (entidad) {
-			try {
-				if (entidad.isEmpty())
-					entidad.wait();
-			} catch (InterruptedException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
-		}
-		return entidad;
 	}
 }
--- src/main/java/utn/algo2/swing/ui/PantallaModificar.java
+++ src/main/java/utn/algo2/swing/ui/PantallaModificar.java
@@ -1,25 +1,17 @@
 package utn.algo2.swing.ui;
 
-import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.lang.reflect.Field;
-import java.util.Hashtable;
 
 import javax.swing.BoxLayout;
 import javax.swing.JButton;
-import javax.swing.JDialog;
 import javax.swing.JLabel;
 import javax.swing.JTextField;
 
 import utn.algo2.core.Entidad;
 
 @SuppressWarnings("serial")
-public class PantallaModificar<T> extends JDialog implements ActionListener {
-
-	private Field[] fields;
-	private Hashtable<Field, JTextField> referenciasATextField = new Hashtable<Field, JTextField>();
-	private Hashtable<String, String> hashConValores = new Hashtable<String, String>();
-	private Entidad entidad = new Entidad();
+public class PantallaModificar<T> extends Pantalla implements ActionListener {
 
 	public PantallaModificar(Field[] fields,
 			Entidad entidadAModificar) {
@@ -48,35 +40,4 @@ public class PantallaModificar<T> extends JDialog implements ActionListener {
 
 		this.configurar();
 	}
-
-	private void configurar() {
-		this.setSize(400, 400);
-		this.setModalityType(ModalityType.APPLICATION_MODAL);
-		this.setVisible(true);
-	}
-
-	public void actionPerformed(ActionEvent e) {
-		synchronized (entidad) {
-			for (Field field : fields) {
-				JTextField textoField = referenciasATextField.get(field);
-				entidad.setValor(field.getName(), textoField.getText());
-			}
-			entidad.notify();
-		}
-		this.setVisible(false);
-	}
-
-	public Entidad getEntidad() {
-		synchronized (entidad) {
-			try {
-				if (entidad.isEmpty())
-					entidad.wait();
-			} catch (InterruptedException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
-		}
-		return entidad;
-	}
-
 }
