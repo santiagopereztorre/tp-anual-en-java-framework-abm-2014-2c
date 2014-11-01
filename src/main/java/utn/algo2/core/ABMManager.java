624a2da9e43d53c6600590c2782a289c8c2e632e
Santiago <saantii.28@hotmail.com>
2014-11-01 20:58:44 -0300

    Gran cambio de responsabilidades La entidad sabe como crear un objeto segun los valores que tiene en el hashtable

--- src/main/java/utn/algo2/core/ABMManager.java
+++ src/main/java/utn/algo2/core/ABMManager.java
@@ -11,13 +11,11 @@ public class ABMManager<T> {
 	private Visualizador<T> visualizador;
 	private Class<?> aClass;
 	private Field[] fields;
-	private Method[] methods;
 
 	public ABMManager(T objeto, Persistidor<T> persistidor,
 			Visualizador<T> visualizador) {
 		this.aClass = objeto.getClass();
 		this.fields = this.aClass.getFields();
-		this.methods = this.aClass.getMethods();
 		this.persistidor = persistidor;
 		this.visualizador = visualizador;
 		this.visualizador.setFields(fields);
@@ -27,82 +25,26 @@ public class ABMManager<T> {
 	 * Ejecuta el ABM Manager
 	 */
 	public void ejecutar() {
-		Entidad entidadCreada = visualizador.pantallaCrear();
+		Entidad<T> entidadCreada = visualizador.pantallaCrear();
 		guardarEntidad(entidadCreada);
 		
-		Entidad entidadAModificar = recuperarEntidad();
+		Entidad<T> entidadAModificar = recuperarEntidad();
 		
-		Entidad entidadModificada = visualizador.pantallaModificar(entidadAModificar);
+		Entidad<T> entidadModificada = visualizador.pantallaModificar(entidadAModificar);
 		guardarEntidad(entidadModificada);
 	}
 
-	private void guardarEntidad(Entidad entidad) {
-		T objeto = crearObjeto(aClass);
-		for (Method method : methods) {
-			if (isSetter(method)) {
-				String nombreVariable = getNombreVariable(method);
-				String valor = entidad.getValor(nombreVariable);
-				try {
-					method.invoke(objeto, valor);
-				} catch (IllegalAccessException e) {
-					e.printStackTrace();
-				} catch (IllegalArgumentException e) {
-					e.printStackTrace();
-				} catch (InvocationTargetException e) {
-					e.printStackTrace();
-				}
-			}
-		}
+	private void guardarEntidad(Entidad<T> entidad) {
+		entidad.setClase(this.aClass);
+		T objeto = entidad.crearObjeto();
 		this.persistidor.guardar(objeto);
 	}
 	
-	private Entidad recuperarEntidad() {
-		Entidad entidad = new Entidad();
+	private Entidad<T> recuperarEntidad() {
+		Entidad<T> entidad = new Entidad<T>();
+		entidad.setClase(this.aClass);
 		T objeto = persistidor.obtener();
-		for (Method method : methods) {
-			if (isGetter(method) && !isGetClass(method)) {
-				String nombreVariable = getNombreVariable(method);
-				String valor = null;
-				try {
-					valor = (String) method.invoke(objeto);
-				} catch (IllegalAccessException e) {
-					e.printStackTrace();
-				} catch (IllegalArgumentException e) {
-					e.printStackTrace();
-				} catch (InvocationTargetException e) {
-					e.printStackTrace();
-				}
-				entidad.setValor(nombreVariable, valor);
-			}
-		}
+		entidad.setObjeto(objeto);
 		return entidad;
-	}
-
-	private String getNombreVariable(Method method) {
-		return method.getName().substring("set".length());
-	}
-
-	private boolean isSetter(Method method) {
-		return method.getName().startsWith("set");
-	}
-
-	private boolean isGetter(Method method) {
-		return method.getName().startsWith("get");
-	}
-
-	private boolean isGetClass(Method method) {
-		return method.getName().startsWith("getClass");
-	}
-
-	@SuppressWarnings("unchecked")
-	private T crearObjeto(Class<?> unaClase) {
-		try {
-			return (T) unaClase.newInstance();
-		} catch (InstantiationException e1) {
-			e1.printStackTrace();
-		} catch (IllegalAccessException e1) {
-			e1.printStackTrace();
-		}
-		return null;
 	}
 }
--- src/main/java/utn/algo2/core/Entidad.java
+++ src/main/java/utn/algo2/core/Entidad.java
@@ -1,10 +1,108 @@
 package utn.algo2.core;
 
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
 import java.util.Hashtable;
 
-public class Entidad {
+public class Entidad<T> {
 
+	private Class<?> unaClase;
+	private T unObjeto;
 	private Hashtable<String, String> hashConValores = new Hashtable<String, String>();
+
+	public Entidad() {
+
+	}
+
+	public Entidad(Class<?> unaClase) {
+		this.unaClase = unaClase;
+	}
+
+	public T crearObjeto() {
+		this.unObjeto = crearInstancia(this.unaClase);
+
+		for (Method method : this.unaClase.getMethods()) {
+			if (isSetter(method)) {
+				String nombreVariable = getNombreVariable(method);
+				String valor = this.getValor(nombreVariable);
+				try {
+					method.invoke(unObjeto, valor);
+				} catch (IllegalAccessException e) {
+					e.printStackTrace();
+				} catch (IllegalArgumentException e) {
+					e.printStackTrace();
+				} catch (InvocationTargetException e) {
+					e.printStackTrace();
+				}
+			}
+		}
+		return unObjeto;
+	}
+
+	@SuppressWarnings("unchecked")
+	private T crearInstancia(Class<?> unaClase) {
+		T objeto = null;
+		try {
+			objeto = (T) unaClase.newInstance();
+		} catch (InstantiationException e1) {
+			e1.printStackTrace();
+		} catch (IllegalAccessException e1) {
+			e1.printStackTrace();
+		}
+		return objeto;
+	}
+
+	private boolean isGetClass(Method method) {
+		return method.getName().startsWith("getClass");
+	}
+
+	private boolean isGetter(Method method) {
+		return method.getName().startsWith("get");
+	}
+
+	private boolean isSetter(Method method) {
+		return method.getName().startsWith("set");
+	}
+
+	private String getNombreVariable(Method method) {
+		return method.getName().substring("set".length());
+	}
+
+	public T getObjeto() {
+		return this.unObjeto;
+	}
+
+	public void setObjeto(T unObjeto) {
+		this.unObjeto = unObjeto;
+		actualizarHashConValores();
+	}
+
+	private void actualizarHashConValores() {
+		for (Method method : this.unaClase.getMethods()) {
+			if (isGetter(method) && !isGetClass(method)) {
+				String nombreVariable = getNombreVariable(method);
+				String valor = null;
+				try {
+					valor = (String) method.invoke(this.unObjeto);
+				} catch (IllegalAccessException e) {
+					e.printStackTrace();
+				} catch (IllegalArgumentException e) {
+					e.printStackTrace();
+				} catch (InvocationTargetException e) {
+					e.printStackTrace();
+				}
+				this.setValor(nombreVariable, valor);
+			}
+		}
+	}
+
+	public Class<?> getClase() {
+		return unaClase;
+	}
+
+	public void setClase(Class<?> unaClase) {
+		this.unaClase = unaClase;
+	}
 
 	public Hashtable<String, String> getHashConValores() {
 		return hashConValores;
@@ -13,11 +111,11 @@ public class Entidad {
 	public void setHashConValores(Hashtable<String, String> hashConValores) {
 		this.hashConValores = hashConValores;
 	}
-	
+
 	public void setValor(String aKey, String aValue) {
 		this.hashConValores.put(aKey.toLowerCase(), aValue);
 	}
-	
+
 	public String getValor(String aKey) {
 		return this.hashConValores.get(aKey.toLowerCase());
 	}
