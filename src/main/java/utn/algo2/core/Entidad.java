b93a6f96276654354197b804ebc5e7b7916af273
Santiago <saantii.28@hotmail.com>
2014-11-05 19:41:09 -0300

    Extraccion de las funciones obtenerGetter y gettearValor

--- src/main/java/utn/algo2/core/Entidad.java
+++ src/main/java/utn/algo2/core/Entidad.java
@@ -33,6 +33,20 @@ public class Entidad<T> {
 			e.printStackTrace();
 		}
 	}
+	
+	private String gettearValor(T unObjeto, Method metodo) {
+		String valor = null;
+		try {
+			valor = (String) metodo.invoke(unObjeto);
+		} catch (IllegalAccessException e) {
+			e.printStackTrace();
+		} catch (IllegalArgumentException e) {
+			e.printStackTrace();
+		} catch (InvocationTargetException e) {
+			e.printStackTrace();
+		}
+		return valor;
+	}
 
 	private Method obtenerSetter(Field field) {
 		Method metodo = null;
@@ -40,6 +54,18 @@ public class Entidad<T> {
 			Class<?>[] args = new Class[1];
 			args[0] = String.class;
 			metodo = this.unaClase.getMethod("set" + capitalize(field.getName()), args);
+		} catch (NoSuchMethodException e) {
+			e.printStackTrace();
+		} catch (SecurityException e) {
+			e.printStackTrace();
+		}
+		return metodo;
+	}
+	
+	private Method obtenerGetter(Field field, Class<?> unaClase) {
+		Method metodo = null;
+		try {
+			metodo = unaClase.getMethod("get" + capitalize(field.getName()), new Class[0]);
 		} catch (NoSuchMethodException e) {
 			e.printStackTrace();
 		} catch (SecurityException e) {
@@ -67,27 +93,8 @@ public class Entidad<T> {
 
 	private void actualizarHashConFields(T unObjeto) {
 		for (Field field : this.unaClase.getFields()) {
-			String valor = null;
-			Class<?>[] args = new Class[0];
-			Method metodo = null;
-			try {
-				metodo = this.unaClase.getMethod("get" + capitalize(field.getName()), args);
-			} catch (NoSuchMethodException e1) {
-				// TODO Auto-generated catch block
-				e1.printStackTrace();
-			} catch (SecurityException e1) {
-				// TODO Auto-generated catch block
-				e1.printStackTrace();
-			}
-			try {
-				valor = (String) metodo.invoke(unObjeto);
-			} catch (IllegalAccessException e) {
-				e.printStackTrace();
-			} catch (IllegalArgumentException e) {
-				e.printStackTrace();
-			} catch (InvocationTargetException e) {
-				e.printStackTrace();
-			}
+			Method metodo = obtenerGetter(field, this.unaClase);
+			String valor = gettearValor(unObjeto, metodo);
 			this.valoresFields.put(field, valor);
 		}
 	}
