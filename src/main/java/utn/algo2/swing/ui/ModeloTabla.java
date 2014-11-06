74bcc104c0c24a5132c1a653ea6757f0ad383e20
Santiago <saantii.28@hotmail.com>
2014-11-06 14:55:21 -0300

    Refactor en ModeloTabla

--- src/main/java/utn/algo2/swing/ui/ModeloTabla.java
+++ src/main/java/utn/algo2/swing/ui/ModeloTabla.java
@@ -1,8 +1,6 @@
 package utn.algo2.swing.ui;
 
 import java.lang.reflect.Field;
-import java.lang.reflect.InvocationTargetException;
-import java.lang.reflect.Method;
 import java.util.List;
 
 import javax.swing.table.AbstractTableModel;
@@ -79,58 +77,21 @@ public class ModeloTabla<T> extends AbstractTableModel {
 			fireTableDataChanged();
 			return;
 		}
-		
 		clase = entidades.get(0).getClase();
-		
-		Field[] atributos = clase.getFields();
-		Integer cantidadAtributos = atributos.length;
+		Field[] fields = clase.getFields();
+		Integer cantidadAtributos = fields.length;
 		Integer cantidadEntidades = entidades.size();
 		Object[][] data = new Object[cantidadEntidades][cantidadAtributos];
 		for (int i = 0; i < cantidadEntidades; i++) {
 			Entidad<T> entidad = entidades.get(i);
-			T objeto = entidad.crearObjeto();
 			Object[] arrayDeAtributos = new Object[cantidadAtributos];
 			for (int j = 0; j < cantidadAtributos; j++) {
-				String nombreDelAtributo = atributos[j].getName();
-				Method metodo = obtenerGetter(clase, nombreDelAtributo);
-				arrayDeAtributos[j] = obtenerValor(objeto, metodo);
+				arrayDeAtributos[j] = entidad.getValor(fields[j]);
 			}
 			data[i] = arrayDeAtributos;
 		}
 		this.data = data;
 		fireTableDataChanged();
-	}
-
-	private String obtenerValor(T objeto, Method metodo) {
-		String valor = null;
-		try {
-			valor = (String) metodo.invoke(objeto);
-		} catch (IllegalAccessException e) {
-			e.printStackTrace();
-		} catch (IllegalArgumentException e) {
-			e.printStackTrace();
-		} catch (InvocationTargetException e) {
-			e.printStackTrace();
-		}
-		return valor;
-	}
-
-	private Method obtenerGetter(Class<?> clase, String nombreDelAtributo) {
-		Method metodo = null;
-		Class<?>[] args = new Class[0];
-		try {
-			metodo = clase.getMethod("get" + capitalize(nombreDelAtributo),
-					args);
-		} catch (NoSuchMethodException e1) {
-			e1.printStackTrace();
-		} catch (SecurityException e1) {
-			e1.printStackTrace();
-		}
-		return metodo;
-	}
-
-	private String capitalize(String line) {
-		return Character.toUpperCase(line.charAt(0)) + line.substring(1);
 	}
 
 	public Entidad<T> getValueAt(int row) {
