06de4c0b9b7ae276507308f48d6d95996dd80238
Santiago <saantii.28@hotmail.com>
2014-11-21 13:54:11 -0300

    Agrego algunas comprobaciones mas a casteador de Localdate

--- src/main/java/utn/algo2/casteadores/LocalDateCasteador.java
+++ src/main/java/utn/algo2/casteadores/LocalDateCasteador.java
@@ -8,6 +8,12 @@ public class LocalDateCasteador implements Casteador {
 
 	@Override
 	public Object castear(String valor) {
+		if (valor.length() != 10)
+			throw new CasteoInvalidoException("Fecha mal ingresada dd-mm-aaaa");
+		
+		if (!valor.substring(2, 3).equals("-") || !valor.substring(5, 6).equals("-"))
+			throw new CasteoInvalidoException("Fecha mal ingresada dd-mm-aaaa");
+		
 		Integer dia = null;
 		Integer mes = null;
 		Integer anio = null;
@@ -16,8 +22,15 @@ public class LocalDateCasteador implements Casteador {
 			mes = Integer.parseInt(valor.substring(3, 5));
 			anio = Integer.parseInt(valor.substring(6, 10));
 		} catch (NumberFormatException e) {
-			throw new CasteoInvalidoException("Mal ingresada la fecha");
+			throw new CasteoInvalidoException("Fecha mal ingresada dd-mm-aaaa");
 		}
+		
+		if (dia > 31 || dia < 0)
+			throw new CasteoInvalidoException("Fecha mal ingresada dd-mm-aaaa");
+		
+		if (mes > 12 || mes < 0)
+			throw new CasteoInvalidoException("Fecha mal ingresada dd-mm-aaaa");
+		
 		return LocalDate.of(anio, mes, dia);
 	}
 
